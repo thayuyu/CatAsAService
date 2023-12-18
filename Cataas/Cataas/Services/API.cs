7737be357ee4b1fa43ee96c2e79d238e5b9b249e
Yunes <yunes1703@icloud.com>
2023-12-18 11:19:45 +0100

    Started the API class

---
+++ Cataas/Cataas/Services/API.cs
@@ -0,0 +1,36 @@
+﻿namespace Cataas.Services
+{
+    public class API
+    {
+        private string Address = "https://cataas.com/cat";
+        // text to be added to picture
+        private string says { get; set; }
+
+        private string type = "square";
+        // filters (mono, negate, custom)
+        private string filter { get; set; }
+        // custom filters: -100 - 100
+        private int brightness { get; set; }
+        private int saturation { get; set; }
+        // custom filters: -180 - 180
+        private int hue { get; set; }
+        // custom filters: 0 - 255
+        private int red { get; set; }
+        private int green { get; set; }
+        private int blue { get; set; }
+        // width and height 
+        private int width = 500;
+        private int height = 500;
+
+        // Constructor
+        public API()
+        {
+
+        }
+
+        public string GetRandomCat()
+        {
+            return Address;
+        }
+    }
+}
