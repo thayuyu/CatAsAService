6c771c10124fd41f4be3872112520ba5b1a28629
Yunes <yunes1703@icloud.com>
2024-01-08 09:20:53 +0100

    Created the API Class and added function GetFilteredCat

--- Cataas/Cataas/Services/API.cs
+++ Cataas/Cataas/Services/API.cs
@@ -10,14 +10,14 @@
         // filters (mono, negate, custom)
         private string filter { get; set; }
         // custom filters: -100 - 100
-        private int brightness { get; set; }
-        private int saturation { get; set; }
+        private int? brightness { get; set; }
+        private int? saturation { get; set; }
         // custom filters: -180 - 180
-        private int hue { get; set; }
+        private int? hue { get; set; }
         // custom filters: 0 - 255
-        private int red { get; set; }
-        private int green { get; set; }
-        private int blue { get; set; }
+        private int? red { get; set; }
+        private int? green { get; set; }
+        private int? blue { get; set; }
         // width and height 
         private int width = 500;
         private int height = 500;
@@ -34,7 +34,5 @@
             Address = $"{Address}?type={type}&width={width}&height={height}";
             return Address;
         }
-
-
     }
 }
