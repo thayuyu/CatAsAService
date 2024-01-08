e646442db43b08b941be83eceb31243053399ec5
Yunes <yunes1703@icloud.com>
2024-01-08 09:23:05 +0100

    Fixed the last commit

--- Cataas/Cataas/Services/API.cs
+++ Cataas/Cataas/Services/API.cs
@@ -3,24 +3,11 @@
     public class API
     {
         private string Address = "https://cataas.com/cat";
-        // text to be added to picture
-        private string says { get; set; }
-
-        private string type = "square";
-        // filters (mono, negate, custom)
-        private string filter { get; set; }
-        // custom filters: -100 - 100
-        private int? brightness { get; set; }
-        private int? saturation { get; set; }
-        // custom filters: -180 - 180
-        private int? hue { get; set; }
-        // custom filters: 0 - 255
-        private int? red { get; set; }
-        private int? green { get; set; }
-        private int? blue { get; set; }
+        private const string type = "square";
+ 
         // width and height 
-        private int width = 500;
-        private int height = 500;
+        private const int width = 500;
+        private const int height = 500;
 
         // Constructor
         public API()
@@ -33,6 +20,50 @@
         {
             Address = $"{Address}?type={type}&width={width}&height={height}";
             return Address;
+        }
+
+
+        public string GetFilterCat(string Filter, string? Says, int? Brightness, int? Saturation, int? Hue, int? Red, int? Green, int? Blue) 
+        {
+
+
+            if (Says == null)
+            {
+                if (Filter == "custom")
+                {
+                    if (Red == null || Green == null || Blue == null) 
+                    {
+                        return Address + $"?type={type}&width={width}&height={height}&filter={Filter}&brightness={Brightness}&saturation={Saturation}&hue={Hue}";
+                    }
+                    else if (Brightness == null || Saturation == null || Hue == null)
+                    {
+                        return Address + $"?type={type}&width={width}&height={height}&filter={Filter}&r={Red}&g={Green}&b={Blue}";
+                    }
+                }
+                else if (Filter == "mono" || Filter == "negate")
+                {
+                    return Address + $"?type={type}&width={width}&height={height}&filter={Filter}";
+                }
+            }
+            else if (Says != null)
+            {
+                if (Filter == "custom")
+                {
+                    if (Red == null || Green == null || Blue == null)
+                    {
+                        return Address + $"/says/{Says}" + $"?type={type}&width={width}&height={height}&filter={Filter}&brightness={Brightness}&saturation={Saturation}&hue={Hue}";
+                    }
+                    else if (Brightness == null || Saturation == null || Hue == null)
+                    {
+                        return Address + $"/says/{Says}" + $"?type={type}&width={width}&height={height}&filter={Filter}&r={Red}&g={Green}&b={Blue}";
+                    }
+                }
+                else if (Filter == "mono" || Filter == "negate")
+                {
+                    return Address + $"/says/{Says}" + $"?type={type}&width={width}&height={height}&filter={Filter}";
+                }
+            }
+            return "https://i.pinimg.com/736x/24/57/04/24570452fbe5544aaf0cae82cab47449.jpg";
         }
     }
 }
