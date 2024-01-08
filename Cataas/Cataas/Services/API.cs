d7fd73bf742551fc945cb245ac8df4f84639a871
Yunes <yunes1703@icloud.com>
2024-01-08 10:26:14 +0100

    Added comments and fixed formatting

--- Cataas/Cataas/Services/API.cs
+++ Cataas/Cataas/Services/API.cs
@@ -2,31 +2,39 @@
 {
     public class API
     {
+        // main URL
         private string Address = "https://cataas.com/cat";
+        // constant variables
+        // type of the picture
         private const string type = "square";
- 
         // width and height 
         private const int width = 500;
         private const int height = 500;
 
         // Constructor
-        public API()
-        {
+        public API() { }
 
-        }
-
-        // Get a random cat with no filters
+        // Get a random cat without filters
         public string GetRandomCat()
         {
             Address = $"{Address}?type={type}&width={width}&height={height}";
             return Address;
         }
 
-
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="Filter">Mono, Negate or Custom</param>
+        /// <param name="Says">Text that will be shown on the picture</param>
+        /// <param name="Brightness">Brightness of the picture</param>
+        /// <param name="Saturation">Saturation of the picture</param>
+        /// <param name="Hue">Hue of the picture</param>
+        /// <param name="Red">RGB option</param>
+        /// <param name="Green">RGB option</param>
+        /// <param name="Blue">RGB option</param>
+        /// <returns>Link of a random filtered cat picture</returns>
         public string GetFilterCat(string Filter, string? Says, int? Brightness, int? Saturation, int? Hue, int? Red, int? Green, int? Blue) 
         {
-
-
             if (Says == null)
             {
                 if (Filter == "custom")
