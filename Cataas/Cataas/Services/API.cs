a0e528ba366706407a9723691d3bf1c716a1e4cf
Yunes <yunes1703@icloud.com>
2024-01-15 10:27:02 +0100

    API class modified and made simple request

--- Cataas/Cataas/Services/API.cs
+++ Cataas/Cataas/Services/API.cs
@@ -18,47 +18,15 @@ namespace Cataas.Services
         // Constructor
         public API() { }
 
-      
-        /// <summary>
-        /// 
-        /// </summary>
-        /// <param name="Filter">Mono, Negate or Custom</param>
-        /// <param name="Brightness">Brightness of the picture</param>
-        /// <param name="Saturation">Saturation of the picture</param>
-        /// <param name="Hue">Hue of the picture</param>
-        /// <param name="Red">RGB option</param>
-        /// <param name="Green">RGB option</param>
-        /// <param name="Blue">RGB option</param>
-        /// <returns>Object "Cat"</returns>
-        public async Task<Cat?> GetCat(string Filter, int? Brightness, int? Saturation, int? Hue, int? Red, int? Green, int? Blue) 
+        public async Task<Cat?> GetCat() 
         {
             Cat? cat = new();
 
             HttpClient client = new();
 
             client.BaseAddress = new Uri(Address);
-
-            string uri = $"?type={type}&width={width}&height={height}&json=true";
-
-            if (Filter == "custom")
-            {
-                uri += $"&filter={Filter}";
-
-                if (Red == null || Green == null || Blue == null)
-                {
-                    uri += $"&brightness={Brightness}&saturation={Saturation}&hue={Hue}";
-                }
-                else if (Brightness == null || Saturation == null || Hue == null)
-                {
-                    uri += $"&r={Red}&g={Green}&b={Blue}";
-                }
-            }
-            else if (Filter == "mono" || Filter == "negate")
-            {
-                uri += $"&filter={Filter}";
-            }
-
-            HttpResponseMessage response = await client.GetAsync(requestUri: uri);
+                        
+            HttpResponseMessage response = await client.GetAsync(requestUri: "?json=true");
 
             if (response.IsSuccessStatusCode) 
             { 
